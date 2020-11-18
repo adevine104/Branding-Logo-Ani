714b6d1a859a4bc2e5be85dcf5e74a9d88d95517
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-11-18 12:10:23 -0500

    commented out

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -13,211 +13,215 @@ const drawInTL = gsap.timeline();
 // drawInTL.from("#WHOLE-THING",{duration:3, x:400, ease:"none"}, "Go")
 //         .to("#WHOLE-THING",{duration:3, x:-400, ease:"none"},"Go");
 
-
-drawInTL.to("#number7", {
-        duration: 1.5,
-        scale:3,
-        transformOrigin: "center"
-    }, "play");
-
-drawInTL.to("#number7", {
-    delay: .5,
-    duration: 1,
-    scale:1,
-    opacity: 0,
-    transformOrigin: "center"
-}, "play2");
-
-drawInTL.to("#line-1", {
-    delay: 1,
-    opacity: 0,
-    transformOrigin: "center"
-}, "play2");
+export function lineAnimation(){
 
 
-drawInTL.from("#line-2", {
-    delay: 1,
-    duration: 1.5,
-    scale: 0,
-    transformOrigin: "center"
-}, "play2");
+    
+    return drawInTL;
+}
 
-drawInTL.to("#line-2", {
-    delay: 1,
-    duration: 1.5,
-    opacity: 1,
-    transformOrigin: "center"
-}, "play2");
 
-// drawInTL.to("#coins", {
+
+// drawInTL.to("#number7", {
+//         duration: 1.5,
+//         scale:3,
+//         transformOrigin: "center"
+//     }, "play");
+
+// drawInTL.to("#number7", {
+//     delay: .5,
+//     duration: 1,
+//     scale:1,
+//     opacity: 0,
+//     transformOrigin: "center"
+// }, "play2");
+
+// drawInTL.to("#line-1", {
+//     delay: 1,
+//     opacity: 0,
+//     transformOrigin: "center"
+// }, "play2");
+
+
+// drawInTL.from("#line-2", {
+//     delay: 1,
 //     duration: 1.5,
+//     scale: 0,
+//     transformOrigin: "center"
+// }, "play2");
+
+// drawInTL.to("#line-2", {
+//     delay: 1,
+//     duration: 1.5,
+//     opacity: 1,
+//     transformOrigin: "center"
+// }, "play2");
+
+// // drawInTL.to("#coins", {
+// //     duration: 1.5,
+// //     opacity: 1,
+// //     // x:1,
+// //     transformOrigin: "center"
+// // }, "play3");
+
+// // drawInTL.from("#coins", {
+// //     duration: 1.5,
+// //     y:25,
+// //     transformOrigin: "bottom"
+// // }, "play3");
+
+// drawInTL.to("#coin-1", {
+//     duration: .5,
 //     opacity: 1,
 //     // x:1,
 //     transformOrigin: "center"
 // }, "play3");
 
-// drawInTL.from("#coins", {
-//     duration: 1.5,
-//     y:25,
+// drawInTL.from("#coin-1", {
+//     duration: .5,
+//     y:5,
 //     transformOrigin: "bottom"
 // }, "play3");
 
-drawInTL.to("#coin-1", {
-    duration: .5,
-    opacity: 1,
-    // x:1,
-    transformOrigin: "center"
-}, "play3");
+// drawInTL.to("#coin-2", {
+//     duration: .5,
+//     opacity: 1,
+//     // x:1,
+//     transformOrigin: "center"
+// }, "play3-1");
 
-drawInTL.from("#coin-1", {
-    duration: .5,
-    y:5,
-    transformOrigin: "bottom"
-}, "play3");
+// drawInTL.from("#coin-2", {
+//     duration: .5,
+//     y:5,
+//     transformOrigin: "bottom"
+// }, "play3-1");
 
-drawInTL.to("#coin-2", {
-    duration: .5,
-    opacity: 1,
-    // x:1,
-    transformOrigin: "center"
-}, "play3-1");
+// drawInTL.to("#coin-3", {
+//     duration: .5,
+//     opacity: 1,
+//     // x:1,
+//     transformOrigin: "center"
+// }, "play3-2");
 
-drawInTL.from("#coin-2", {
-    duration: .5,
-    y:5,
-    transformOrigin: "bottom"
-}, "play3-1");
-
-drawInTL.to("#coin-3", {
-    duration: .5,
-    opacity: 1,
-    // x:1,
-    transformOrigin: "center"
-}, "play3-2");
-
-drawInTL.from("#coin-3", {
-    duration: .5,
-    y:5,
-    transformOrigin: "bottom"
-}, "play3-2");
+// drawInTL.from("#coin-3", {
+//     duration: .5,
+//     y:5,
+//     transformOrigin: "bottom"
+// }, "play3-2");
 
 
 
-drawInTL.to("#line-2", {
-    delay: 1,
-    opacity: 0,
-    transformOrigin: "center"
-}, "play4");
+// drawInTL.to("#line-2", {
+//     delay: 1,
+//     opacity: 0,
+//     transformOrigin: "center"
+// }, "play4");
 
-drawInTL.from("#line-3", {
-    delay: 1,
-    duration: 1.5,
-    scale: 0,
-    transformOrigin: "center"
-}, "play4");
-
-drawInTL.to("#line-3", {
-    delay: 1,
-    duration: 1.5,
-    opacity: 1,
-    transformOrigin: "center"
-}, "play4");
-
-
-// drawInTL.to("#tech", {
+// drawInTL.from("#line-3", {
+//     delay: 1,
 //     duration: 1.5,
+//     scale: 0,
+//     transformOrigin: "center"
+// }, "play4");
+
+// drawInTL.to("#line-3", {
+//     delay: 1,
+//     duration: 1.5,
+//     opacity: 1,
+//     transformOrigin: "center"
+// }, "play4");
+
+
+// // drawInTL.to("#tech", {
+// //     duration: 1.5,
+// //     opacity: 1,
+// //     // x:1,
+// //     transformOrigin: "center"
+// // }, "play5");
+
+// // drawInTL.from("#tech", {
+// //     duration: 1,
+// //     rotate:-30,
+// //     transformOrigin: "bottom center"
+// // }, "play5");
+
+// drawInTL.to("#ipad", {
+//     duration: 1,
 //     opacity: 1,
 //     // x:1,
 //     transformOrigin: "center"
 // }, "play5");
 
-// drawInTL.from("#tech", {
-//     duration: 1,
+// drawInTL.from("#ipad", {
+//     duration: .5,
 //     rotate:-30,
 //     transformOrigin: "bottom center"
 // }, "play5");
 
-drawInTL.to("#ipad", {
-    duration: 1,
-    opacity: 1,
-    // x:1,
-    transformOrigin: "center"
-}, "play5");
+// drawInTL.to("#iphone", {
+//     duration: .5,
+//     opacity: 1,
+//     // x:1,
+//     transformOrigin: "center"
+// }, "play5-1");
 
-drawInTL.from("#ipad", {
-    duration: .5,
-    rotate:-30,
-    transformOrigin: "bottom center"
-}, "play5");
-
-drawInTL.to("#iphone", {
-    duration: .5,
-    opacity: 1,
-    // x:1,
-    transformOrigin: "center"
-}, "play5-1");
-
-drawInTL.from("#iphone", {
-    duration: .75,
-    rotate:-30,
-    transformOrigin: "bottom center"
-}, "play5-1");
+// drawInTL.from("#iphone", {
+//     duration: .75,
+//     rotate:-30,
+//     transformOrigin: "bottom center"
+// }, "play5-1");
 
 
-drawInTL.to("#line-3", {
-    delay: 1,
-    opacity: 0,
-    transformOrigin: "center"
-}, "play6");
+// drawInTL.to("#line-3", {
+//     delay: 1,
+//     opacity: 0,
+//     transformOrigin: "center"
+// }, "play6");
 
-drawInTL.from("#tagline", {
-    delay: 1,
-    duration: 1.5,
-    scale: 0,
-    transformOrigin: "center"
-}, "play6");
+// drawInTL.from("#tagline", {
+//     delay: 1,
+//     duration: 1.5,
+//     scale: 0,
+//     transformOrigin: "center"
+// }, "play6");
 
-drawInTL.to("#RB", {
-    delay: 1,
-    duration: 1.5,
-    opacity: 1,
-    transformOrigin: "center"
-}, "play6");
+// drawInTL.to("#RB", {
+//     delay: 1,
+//     duration: 1.5,
+//     opacity: 1,
+//     transformOrigin: "center"
+// }, "play6");
 
-drawInTL.from("#RepublicBank", {
-    duration: 3,
-    x:25,
-    transformOrigin: "center"
-}, "play7");
+// drawInTL.from("#RepublicBank", {
+//     duration: 3,
+//     x:25,
+//     transformOrigin: "center"
+// }, "play7");
 
-drawInTL.to("#RepublicBank", {
-    duration: 3,
-    opacity: 1,
-    transformOrigin: "center"
-}, "play7");
+// drawInTL.to("#RepublicBank", {
+//     duration: 3,
+//     opacity: 1,
+//     transformOrigin: "center"
+// }, "play7");
 
-drawInTL.from("#tagline", {
-    duration: 3,
-    x:-25,
-    transformOrigin: "center"
-}, "play7");
+// drawInTL.from("#tagline", {
+//     duration: 3,
+//     x:-25,
+//     transformOrigin: "center"
+// }, "play7");
 
-drawInTL.to("#tagline", {
-    duration: 3,
-    opacity: 1,
-    transformOrigin: "center"
-}, "play7");
+// drawInTL.to("#tagline", {
+//     duration: 3,
+//     opacity: 1,
+//     transformOrigin: "center"
+// }, "play7");
 
-drawInTL.to("#R", {
-    duration: 1,
-    fill:"#EF5556",
-    transformOrigin: "center"
-}, "play7");
+// drawInTL.to("#R", {
+//     duration: 1,
+//     fill:"#EF5556",
+//     transformOrigin: "center"
+// }, "play7");
 
-
-export function lineAnimation(){
-    return drawInTL;
-}
 
 
 
